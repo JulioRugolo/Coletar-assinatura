29d82a45abf5303d38f7adb04809762e433e3d54
julio.rugolo <juliorugolo@gmail.com>
2024-07-31 10:03:05 -0300

    remover botao de email

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ document.getElementById('save').addEventListener('click', () => {
     const dataURL = signaturePad.toDataURL('image/png');
     const link = document.createElement('a');
     link.href = dataURL;
-    link.download = 'assinatura.png';
+    link.download = 'Assinatura.png';
     link.click();
   }
 });
@@ -18,17 +18,4 @@ document.getElementById('save').addEventListener('click', () => {
 // Botão para limpar a assinatura
 document.getElementById('clear').addEventListener('click', () => {
   signaturePad.clear();
-});
-
-// Botão para enviar a assinatura por email
-document.getElementById('send-email').addEventListener('click', () => {
-  if (signaturePad.isEmpty()) {
-    alert("Por favor, desenhe sua assinatura primeiro.");
-  } else {
-    const dataURL = signaturePad.toDataURL('image/png');
-    const email = prompt("Por favor, insira o email para enviar a assinatura:");
-    if (email) {
-      window.location.href = `mailto:${email}?subject=Assinatura&body=Segue em anexo a assinatura.&attachment=${encodeURIComponent(dataURL)}`;
-    }
-  }
 });
